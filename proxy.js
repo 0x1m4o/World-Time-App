18575173573b86d565d92b4ac2606d2c55264fa9
0x1m4o <chuntdevnx@gmail.com>
2023-05-25 03:19:37 +0700

    Hopefully Final Commit

--- proxy.js
+++ proxy.js
@@ -22,6 +22,16 @@ module.exports = async (req, res) => {
     }
 
     // Forward the response from the API server to the client
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+    );
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+    );
+
     res.status(response.status).json(response.data);
   } catch (error) {
     // Handle errors
