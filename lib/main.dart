b0d620e73672da2d63ce90b3e88da005426e634d
0x1m4o <chuntdevnx@gmail.com>
2023-05-25 19:36:09 +0700

    Update Main

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,48 @@ void main() {
         '/': (context) => Loading(),
         '/home': (context) => Home(),
         '/location': (context) => ChooseLocation(),
+        '/api/time': (context) => ApiTimeHandler(),
+        '/api/timezone': (context) =>
+            ApiTimeZoneHandler(), // New route handler for /api/timezone
       },
     ),
   ));
+}
+
+class ApiTimeHandler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Map<String, dynamic> params =
+        ModalRoute.of(context)?.settings.arguments as Map<String, dynamic>;
+
+    final String? timeZone = params['timeZone'];
+
+    if (timeZone == null) {
+      return Scaffold(
+        body: Center(
+          child: Text('Missing timeZone query parameter'),
+        ),
+      );
+    }
+
+    return Scaffold(
+      body: Center(
+        child: Text('Handle API call for timeZone: $timeZone'),
+      ),
+    );
+  }
+}
+
+class ApiTimeZoneHandler extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Handle the API call for /api/timezone
+    // You can use the WorldTime class or any other approach to handle the logic
+
+    return Scaffold(
+      body: Center(
+        child: Text('Handle API call for /api/timezone'),
+      ),
+    );
+  }
 }
