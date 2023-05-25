abccf105dd3a3ad41066bec5e2182e66b5396d7e
0x1m4o <chuntdevnx@gmail.com>
2023-05-25 19:46:12 +0700

    Update Main

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:world_time/blocs/search_filter/search_filter_bloc.dart';
@@ -5,6 +7,7 @@ import 'package:world_time/blocs/world_time_list/world_time_list_bloc.dart';
 import 'package:world_time/pages/choose_location.dart';
 import 'package:world_time/pages/home.dart';
 import 'package:world_time/pages/loading.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(MultiBlocProvider(
@@ -49,22 +52,76 @@ class ApiTimeHandler extends StatelessWidget {
 
     return Scaffold(
       body: Center(
-        child: Text('Handle API call for timeZone: $timeZone'),
+        child: FutureBuilder(
+          future: fetchTimeData(timeZone),
+          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              return Text('Time Data: ${snapshot.data}');
+            }
+          },
+        ),
       ),
     );
+  }
+
+  Future<String> fetchTimeData(String timeZone) async {
+    final url = 'https://timeapi.io/api/Time/current/zone?timeZone=$timeZone';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return data['dateTime'];
+    } else {
+      throw Exception('Failed to fetch time data');
+    }
   }
 }
 
 class ApiTimeZoneHandler extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Handle the API call for /api/timezone
-    // You can use the WorldTime class or any other approach to handle the logic
-
     return Scaffold(
       body: Center(
-        child: Text('Handle API call for /api/timezone'),
+        child: FutureBuilder(
+          future: fetchTimeZones(),
+          builder:
+              (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            } else {
+              final timeZones = snapshot.data;
+              return ListView.builder(
+                itemCount: timeZones!.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text(timeZones[index]),
+                  );
+                },
+              );
+            }
+          },
+        ),
       ),
     );
+  }
+
+  Future<List<String>> fetchTimeZones() async {
+    final url = 'https://timeapi.io/api/TimeZone/AvailableTimeZones';
+
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return List<String>.from(data);
+    } else {
+      throw Exception('Failed to fetch time zones');
+    }
   }
 }
